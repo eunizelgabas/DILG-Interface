f321b6e1f7b74d05439f8b61553eaf6a498a472e
eunizelgabas <eunizelgabas24@gmail.com>
2024-01-30 10:04:43 +0800

    minor change (2)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>DILG Webiste</title>
+        @vite('resources/css/app.css')
+        <title>DILG Issuance</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
