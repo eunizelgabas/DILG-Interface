68b755b661dcdfb1004b15712c2f84ddc8473ee5
eunizelgabas <eunizelgabas24@gmail.com>
2024-01-30 13:28:56 +0800

    login api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\DraftController;
 use App\Http\Controllers\IssuanceController;
 use App\Http\Controllers\JointController;
 use App\Http\Controllers\LegalController;
+use App\Http\Controllers\MemoController;
+use App\Http\Controllers\PresidentialController;
+use App\Http\Controllers\RepublicController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,13 +26,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', function() {
-    return response([
-        'message' => 'Api is working'
-    ], 200);
-});
+Route::post('/auth/login', [UserController::class, 'login']);
+
+// Route::get('/test', function() {
+//     return response([
+//         'message' => 'Api is working'
+//     ], 200);
+// });
 
 Route::get('/latest_issuances', [IssuanceController::class, 'index']);
 Route::get('/joint_circulars', [JointController::class, 'index']);
 Route::get('/legal_opinions', [LegalController::class, 'index']);
+Route::get('/memo_circulars', [MemoController::class, 'index']);
+Route::get('/presidential_directives', [PresidentialController::class, 'index']);
+Route::get('/draft_issuances', [DraftController::class, 'index']);
+Route::get('/republic_acts', [RepublicController::class, 'index']);
+// Route::get('/legal_opinions', [LegalController::class, 'index']);
 // Route::get('/api/download_issuance/{issuance}', [IssuanceController::class, 'downloadIssuanceFile']);
